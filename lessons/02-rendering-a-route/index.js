7638a22322607f79b2132f8b7711fb52c9fd363b
Kai Chen <kchen9530@gmail.com>
2018-05-05 19:31:18 -0400

    add simple router

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import { Router, Route, hashHistory } from 'react-router'
+// insert into index.js
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+// render(<App/>, document.getElementById('app'))
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App} />
+        {/* add the routes here */}
+        <Route path="/repos" component={Repos} />
+        <Route path="/about" component={About} />
+    </Router>
+), document.getElementById('app'))
