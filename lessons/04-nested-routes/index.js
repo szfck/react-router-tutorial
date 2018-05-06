40e9869d89d9a714d41d9ccd48fbd049eee492dd
Kai Chen <kchen9530@gmail.com>
2018-05-05 20:13:14 -0400

    add route children

--- lessons/04-nested-routes/index.js
+++ lessons/04-nested-routes/index.js
@@ -6,9 +6,10 @@ import About from './modules/About'
 import Repos from './modules/Repos'
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="/repos" component={Repos} />
+            <Route path="/about" component={About} />
+        </Route>
+    </Router>
 ), document.getElementById('app'))
