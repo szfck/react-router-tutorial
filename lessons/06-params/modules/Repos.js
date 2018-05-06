2bbaa83e10644cf6d6deab22378049deaa5070e2
Kai Chen <kchen9530@gmail.com>
2018-05-05 20:46:40 -0400

    add url params

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
-  render() {
-    return <div>Repos</div>
-  }
+    render() {
+        return (
+            <div>
+                <div>Repos</div>
+                <ul>
+                    <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
+                    <li><Link to="/repos/facebook/react">React</Link></li>
+                </ul>
+            </div>
+        )
+    }
 })
