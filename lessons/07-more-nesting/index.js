2f102783d45717f7ac7db5d649f992bf34017c21
Kai Chen <kchen9530@gmail.com>
2018-05-05 20:51:46 -0400

    add nested route

--- lessons/07-more-nesting/index.js
+++ lessons/07-more-nesting/index.js
@@ -7,11 +7,12 @@ import Repos from './modules/Repos'
 import Repo from './modules/Repo'
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/repos" component={Repos}/>
-      <Route path="/repos/:userName/:repoName" component={Repo}/>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="/repos" component={Repos}>
+                <Route path="/repos/:userName/:repoName" component={Repo} />
+            </Route>
+            <Route path="/about" component={About} />
+        </Route>
+    </Router>
 ), document.getElementById('app'))
